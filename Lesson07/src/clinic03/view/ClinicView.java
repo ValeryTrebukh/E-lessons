88c24514ecca8d0bae29c80e81f2d3886acf5edf
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-04-06 00:42:18 +0300

    task 7 - internationalization

--- Lesson07/src/clinic03/view/ClinicView.java
+++ Lesson07/src/clinic03/view/ClinicView.java
@@ -3,9 +3,15 @@ package clinic03.view;
 import clinic03.model.Patient;
 import org.apache.log4j.Logger;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class ClinicView {
 
     private static Logger rootLogger = Logger.getRootLogger();
+
+//    ResourceBundle bundle = ResourceBundle.getBundle("location.lang", new Locale("ru", "RU"));
+    ResourceBundle bundle = ResourceBundle.getBundle("location.lang");
 
     public void print(Patient [] patients) {
 
@@ -27,8 +33,11 @@ public class ClinicView {
     }
 
 
-    public void printMenu() {
+   /* public void printMenu() {
         System.out.println("\n1 - SELECT BY DIAGNOSIS    2 - SELECT BY ID    " +
                 "3 - SORT BY NAME    4 - LOAD    5 - SAVE    0 - EXIT");
+    }*/
+    public void printMenu() {
+        System.out.println(bundle.getString("menu"));
     }
 }
