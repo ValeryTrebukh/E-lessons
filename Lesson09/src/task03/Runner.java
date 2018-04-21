3b60f2265234350a71990680456d84944a5ff7fd
Valery Trebukh <valery.m.trebukh@gmail.com>
2018-04-21 21:08:59 +0300

    Lesson09.task03 time calc fixed

--- Lesson09/src/task03/Runner.java
+++ Lesson09/src/task03/Runner.java
@@ -4,73 +4,99 @@ import java.util.AbstractMap;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
 
 class Runner {
     private AbstractMap<Integer, String> map;
     private Runnable sw, sr, cw, cr;
-    private final int NUM_THREADS = 4;
-    private final int MAP_SIZE = 4_000_000;
+    private final int NUM_THREADS = 16;
+    private final int MAP_SIZE = 5_000_000;
+    private AtomicLong syncWriteTime = new AtomicLong();
+    private AtomicLong syncReadTime = new AtomicLong();
+    private AtomicLong concWriteTime = new AtomicLong();
+    private AtomicLong concReadTime = new AtomicLong();
 
     {
          sw = () -> {
              int i = 0;
-             synchronized (this) {
-                 while (i < MAP_SIZE) {
-                     map.put(i++, Thread.currentThread().getName());
+             long t;
+             long threadTime = 0;
+             while (i++ < MAP_SIZE) {
+                 t = new Date().getTime();
+                 synchronized (this) {
+                     map.put(i, Thread.currentThread().getName());
                  }
+                 threadTime += new Date().getTime() - t;
              }
+             syncWriteTime.addAndGet(threadTime);
         };
 
         sr = () -> {
             int i = 0;
-            synchronized (this) {
-                while (i < MAP_SIZE) {
-                    map.get(i++);
+            long t;
+            long threadTime = 0;
+            while (i++ < MAP_SIZE) {
+                t = new Date().getTime();
+                synchronized (this) {
+                    map.get(i);
+
                 }
-                map.clear();
+                threadTime += new Date().getTime() - t;
             }
+            syncReadTime.addAndGet(threadTime);
         };
 
         cw = () -> {
             int i = 0;
-            while (i < MAP_SIZE) {
-                map.put(i++, Thread.currentThread().getName());
+            long t;
+            long threadTime = 0;
+            while (i++ < MAP_SIZE) {
+                t = new Date().getTime();
+                map.put(i, Thread.currentThread().getName());
+                threadTime += new Date().getTime() - t;
             }
+            concWriteTime.addAndGet(threadTime);
         };
 
         cr = () -> {
             int i = 0;
-            while (i < MAP_SIZE) {
-                map.get(i++);
+            long t;
+            long threadTime = 0;
+            while (i++ < MAP_SIZE) {
+                t = new Date().getTime();
+                map.get(i);
+                threadTime += new Date().getTime() - t;
             }
-            map.clear();
+            concReadTime.addAndGet(threadTime);
         };
     }
 
 
     void run() throws InterruptedException {
+        long t;
+        System.out.println("Map size: " + MAP_SIZE + ". Threads number: " + NUM_THREADS);
+        System.out.println();
 
         map = new HashMap<>(1000, 0.8f);
-        Long s1;
-
-        s1 = new Date().getTime();
+        t = new Date().getTime();
         startThreads(sw);
-        System.out.println("synchronized write time: " + (new Date().getTime() - s1));
-
-        s1 = new Date().getTime();
+        System.out.println("main\tsynchronized write time:\t" + (new Date().getTime() - t));
+        System.out.println("thread\tsynchronized write time:\t" + syncWriteTime.get()/NUM_THREADS);
+        t = new Date().getTime();
         startThreads(sr);
-        System.out.println("synchronized read time: " + (new Date().getTime() - s1));
-
+        System.out.println("main\tsynchronized read  time:\t" + (new Date().getTime() - t));
+        System.out.println("thread\tsynchronized read  time:\t" + syncReadTime.get()/NUM_THREADS);
+        System.out.println();
 
         map = new ConcurrentHashMap<>(1000, 0.8f);
-
-        s1 = new Date().getTime();
+        t = new Date().getTime();
         startThreads(cw);
-        System.out.println("concurrent write time: " + (new Date().getTime() - s1));
-
-        s1 = new Date().getTime();
+        System.out.println("main\tconcurrent write time:\t" + (new Date().getTime() - t));
+        System.out.println("thread\tconcurrent write time:\t" + concWriteTime.get()/NUM_THREADS);
+        t = new Date().getTime();
         startThreads(cr);
-        System.out.println("concurrent read time: " + (new Date().getTime() - s1));
+        System.out.println("main\tconcurrent read  time:\t" + (new Date().getTime() - t));
+        System.out.println("thread\tconcurrent read  time:\t" + concReadTime.get()/NUM_THREADS);
 
     }
 
